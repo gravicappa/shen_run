0396433c4cfb5b053d0d0e9245f116f4993543ac
Ramil Farkhshatov <ramil@gmx.co.uk>
2012-08-24 00:42:38 +0400

    Unable to run from Shen CL functions in packages other than CL.

--- config.def.h
+++ config.def.h
@@ -8,10 +8,10 @@ char *confname = ".shen.shen";
 #define ERRFILE "shen.err"
 
 /* command to exit
-  on Shen SBCL it's (SB-UNIX:EXIT Ecode)
+  on Shen SBCL it's (QUIT)
   on Shen CLisp it's (EXIT Ecode)
   on Shen JS it's (shenjs-exit Ecode) */
-char *exit_expr = "(SB-UNIX:EXIT Ecode)\n";
+char *exit_expr = "(QUIT)\n";
 
 char *start_expr = "(set shen-*history* [])\n";
 char *main_func = "main";
